a2446faa94b58fcb0a68faff67ada0ebe5f3c7dc
Nic Headlee <nicheadlee@gmail.com>
2019-12-27 16:10:39 -0700

    rolodex updates

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component'
 
 // Making a comment here for a git test
 
@@ -21,15 +22,24 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
         <div className='App'>
-          <input 
-            type='search' 
-            placeholder='search monsters' 
-            onChange={e => this.setState({ searchField: e.target.value })}
+          <h1>Monsters Rolodex</h1>
+          <SearchBox 
+            placeholder='search monsters'
+            handleChange={this.handleChange}
           />
-          <CardList monsters={this.state.monsters} /> 
+          <CardList monsters={filteredMonsters} /> 
         </div>
       );
     }
